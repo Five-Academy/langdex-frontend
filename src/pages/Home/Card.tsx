c3f5bc67eeb7ecd4ffaf831849ab76d12d27482f
Luiz Henrique Hipolito dos Santos Wrubleski <lwrubleski@vertigo.com.br>
2021-10-05 11:01:29 -0300

    Iniciado o desenvolvimento do routing por carta

--- src/pages/Home/Card.tsx
+++ src/pages/Home/Card.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.scss';
 
 type Props = {
-  content: Array<string>;
+  content: contentProp;
   number: number;
 };
 
+type contentProp = {
+  createdAt: string;
+  id: string;
+};
+
 const Card = ({ content, number }: Props): JSX.Element => (
-  <div className="card">
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
-      className="img"
-      alt="#"
-    />
-    <div className="div">
-      <h2 className="number">#001</h2>
-      <div className="text-container">
-        <p className="text">Java Script blablabla</p>
+  <Link
+    to={`language/${content.id}`}
+    style={{ textDecoration: 'none', color: 'inherit' }}
+  >
+    <div className="card">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/480px-Unofficial_JavaScript_logo_2.svg.png"
+        className="img"
+        alt="#"
+      />
+      <div className="div">
+        <h2 className="number">#001</h2>
+        <div className="text-container">
+          <p className="text">Java Script blablabla</p>
+        </div>
       </div>
     </div>
-  </div>
+  </Link>
 );
 
 export default Card;
